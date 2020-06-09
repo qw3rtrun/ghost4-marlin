3241cac472c3c0b66c932bb4788786e10d6b76dc
qw3rtrun <qw3rtrun@gmail.com>
2020-06-09 19:39:16 +0300

    Extruder configuration for BMG

--- Configuration.h
+++ Configuration.h
@@ -617,14 +617,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80,80,400,94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80,80,400,300 }
 
  /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 150, 150, 20, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 150, 150, 20, 150 }
 
  /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -865,7 +865,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR true
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
